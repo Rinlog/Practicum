27934dcb4e19e23b49ec404fa5addadbf3cf53f1
Rinlog <Sadikulz123@gmail.com>
2025-06-12 14:49:43 -0300

    added small home page section so it doesn't look as empty

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -4,11 +4,46 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Ramsey\Uuid\Uuid;
+use \Exception;
 #[Title("Home | IDL")]
 class Home extends Component
 {
+    public $user;
+    public $userRoles = [];
+    public function LoadUsersRoles(){
+        try{
+            $roleAssoc = DB::table("user_role_association")->where("user_id",$this->user->user_id)->get();
+            $roleIds = [];
+            foreach ($roleAssoc as $role){
+                array_push($roleIds,$role->role_id);
+            }
+            $this->userRoles = DB::table("role")->whereIn("role_id",$roleIds)->get();
+
+        }
+        catch(Exception $e){
+            Log::channel("customlog")->error($e->getMessage());
+        }
+    }
+    public function LoadUserInfo(){
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (isset($_SESSION["User"])) {
+            try{
+                $this->user = $_SESSION["User"];
+            }
+            catch(Exception $e){
+                $this->user = "";
+            }
+        }
+    }
     public function render()
     {
+        $this->LoadUserInfo();
+        $this->LoadUsersRoles();
         return view('livewire.home');
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,4 +1,6 @@
-<?php session_start()?>
+<?php
+include_once(app_path() . "/Includes/OutPutIfLoggedIn.php");
+?>
 <div class="bg-[#f2f2f2] flex flex-row">
     @vite('resources/css/home.css')
     @if (empty($_SESSION["UserName"]))
@@ -8,5 +10,19 @@
     {{-- main section --}}
     <div class="w-screen flex flex-col flex-wrap">
         <livewire:usercontrols.usercontrolnav></livewire:usercontrols.usercontrolnav>
+        <div class="p-10">
+            <div class="bg-[#00719d] w-[45%] rounded-lg p-10 flex flex-col gap-4">
+                <h1 class="text-white">Hello, <b><?php OutPutIfLoggedIn("UserName");?>!</b></h1>
+                <span class="text-[#48bbe7] text-lg font-semibold">
+                    @foreach( $userRoles as $role)
+                        @if ($loop->index == 0)
+                            {{ strtoupper($role->role_name) }}
+                        @else
+                            {{", " . strtoupper($role->role_name) }}
+                        @endif
+                    @endforeach
+                </span>
+            </div>
+        </div>
     </div>
 </div>
